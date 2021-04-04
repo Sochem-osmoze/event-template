a60c6a37144a111c3360390fc8de3630da53ed4d
varunsahu29.vs <varunsahu29.vs@gmail.com>
2021-04-04 16:39:21 +0530

    t

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+// import {Progress, Alert} from 'reactstrap';
+import {withRouter} from 'react-router-dom';
+import {dismissAlert} from '../../actions/alerts';
+import s from './Sidebar.module.scss';
+import LinksGroup from './LinksGroup';
+
+import {changeActiveSidebarItem} from '../../actions/navigation';
+import {logoutUser} from '../../actions/user';
+import HomeIcon from '../Icons/SidebarIcons/HomeIcon';
+// import TypographyIcon from '../Icons/SidebarIcons/TypographyIcon';
+// import TablesIcon from '../Icons/SidebarIcons/TablesIcon';
+// import NotificationsIcon from '../Icons/SidebarIcons/NotificationsIcon';
+// import ComponentsIcon from '../Icons/SidebarIcons/ComponentsIcon';
+
+
+
+class Sidebar extends React.Component {
+    static propTypes = {
+        sidebarStatic: PropTypes.bool,
+        sidebarOpened: PropTypes.bool,
+        dispatch: PropTypes.func.isRequired,
+        activeItem: PropTypes.string,
+        location: PropTypes.shape({
+            pathname: PropTypes.string,
+        }).isRequired,
+    };
+
+    static defaultProps = {
+        sidebarStatic: false,
+        activeItem: '',
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.doLogout = this.doLogout.bind(this);
+    }
+
+    componentDidMount() {
+        this.element.addEventListener('transitionend', () => {
+            if (this.props.sidebarOpened) {
+                this.element.classList.add(s.sidebarOpen);
+            }
+        }, false);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.sidebarOpened !== this.props.sidebarOpened) {
+            if (nextProps.sidebarOpened) {
+                this.element.style.height = `${this.element.scrollHeight}px`;
+            } else {
+                this.element.classList.remove(s.sidebarOpen);
+                setTimeout(() => {
+                    this.element.style.height = '';
+                }, 0);
+            }
+        }
+    }
+
+    dismissAlert(id) {
+        this.props.dispatch(dismissAlert(id));
+    }
+
+    doLogout() {
+        this.props.dispatch(logoutUser());
+    }
+
+    render() {
+        return (
+            <nav
+                className={cx(s.root)}
+                ref={(nav) => {
+                    this.element = nav;
+                }}
+            >
+                <header className={s.logo}>
+                    <a href="https://demo.flatlogic.com/light-blue-react/">Light <span
+                        className="fw-bold">Blue</span></a>
+                </header>
+                <ul className={s.nav}>
+                    <LinksGroup
+                        onActiveSidebarItemChange={activeItem => this.props.dispatch(changeActiveSidebarItem(activeItem))}
+                        activeItem={this.props.activeItem}
+                        header="Dashboard"
+                        isHeader
+                        iconName={<HomeIcon className={s.menuIcon} />}
+                        link="/app/main"
+                        index="main"
+                    />
+                    <LinksGroup
+                        onActiveSidebarItemChange={activeItem => this.props.dispatch(changeActiveSidebarItem(activeItem))}
+                        activeItem={this.props.activeItem}
+                        header="ChemCoding"
+                        isHeader
+                        iconName={<HomeIcon className={s.menuIcon} />}
+                        link="/app/chemCoding"
+                        index="chemcoding"
+                    />
+                    
+                   
+                </ul>
+                
+
+            </nav>
+        );
+    }
+}
+
+function mapStateToProps(store) {
+    return {
+        sidebarOpened: store.navigation.sidebarOpened,
+        sidebarStatic: store.navigation.sidebarStatic,
+        alertsList: store.alerts.alertsList,
+        activeItem: store.navigation.activeItem,
+    };
+}
+
+export default withRouter(connect(mapStateToProps)(Sidebar));
---
+++ src/pages/chemcoding/chemcoding.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Row, Col } from "reactstrap";
+
+import Widget from "../../components/Widget/Widget";
+
+const ChemCoding = () => (
+  <div >
+    {/* This is New Widget */}
+    <Row>
+      <Col xs={12} lg={6}>
+        <Widget
+          title={
+            <h5>
+              chemcoding{" "}
+              <small className="text-muted">25 March 2020</small>
+            </h5>
+          }
+          collapse
+          fullscreen
+
+
+        >
+          <h4 className="mt-5  display-4">ChemCoding Details </h4>
+          <div className="widget-padding-md w-100 h-100 text-left border rounded">
+            <p className="lead">
+            Information Technology today is growing at an impeccable rate and as such being able to code is one such skill which would go a really long way in one's career, regardless of profession. Osmoze brings to you the opportunity to showcase logical thinking and dexterity with algorithms in the form of ChemCoding, an exhilarating coding event.
+            </p>
+            <p className="lead">
+            ChemCoding is a competitive programming event that will test your problem solving and programming skills. You will be given a series of algorithmic puzzles and problems and you will have to come up with not only an accurate but an optimal solution as quickly as possible.
+            </p>
+            <p className="lead">
+            <strong>Registrations will begin on 26th March</strong>
+            </p>
+            <p className="lead">
+            Event Coordinators:
+            <ul>
+              <li>Nisha Agarwal: 9521409751</li>
+              <li>Abhishek Singh: 9460878830</li>
+            </ul>
+            <div class="timeline-footer">
+            <a href="https://www.sochem.org" class="btn btn-primary btn-sm">SoChem Website</a><br></br><br></br>
+            <a href="https://www.osmoze.in" class="btn btn-danger btn-sm">Osmoze Home</a>
+          </div>
+            </p>
+          </div>
+        </Widget>
+      </Col>
+    </Row>
+
+    {/* This is New Widget */}
+    <Row>
+      <Col xs={12} lg={6}>
+        <Widget
+          title={
+            <h5>
+              chemcoding{" "}
+              <small className="text-muted">24 march 2021</small>
+            </h5>
+          }
+          collapse
+          fullscreen
+
+
+        >
+          <h4 className="mt-5  display-4">ChemCoding has turned un-proctored</h4>
+          <div className="widget-padding-md w-100 h-100 text-left border rounded">
+            <p className="lead">
+                As a part of the decision taken today by the Osmoze Core team, all events have turned online. The new cash prize for Chem Coding will be INR. 5000.
+               </p>
+
+          </div>
+        </Widget>
+      </Col>
+    </Row>
+    
+    <iframe src="https://www.google.com" ></iframe>
+  </div>
+);
+
+export default ChemCoding;
